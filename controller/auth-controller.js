68252ab8b0b5fba25fb91d2aa00cc8f1ef77aa22
Jesper Sandberg Hesselgren <jespersh@kth.se>
2025-04-10 09:55:42 +0200

    feat: signing up user and storing extra data about user in database done.

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,29 +1,45 @@
-export const registerUser = (req, res) => {
-    const { name, lastName, ssn, email, phoneNumber, password, address } = req.body;
+import { supabase } from "../config/supabaseConfig.js";
+
+// Register user on path /api/auth/register
+export const registerUser = async (req, res) => {
+    const { user_id, name, lastname, pnummer, email, phone, adress, city, zip } = req.body;
+
+    console.log(req.body);
   
-    if (!name || !email || !phoneNumber || !password || !lastName || !password || !ssn || !address) {
+    if (!user_id || !name || !lastname || !email || !phone || !pnummer|| !adress || !city || !zip) {
+      console.log("Missing required fields");
       return res.status(400).json({ message: "All fields are required" });
-      console.log("All fields are required");
     }
+
   
     try {
+      // Saves extra user data to table AuthenticatedUser
+      const { error } = await supabase
+      .from('AuthenticatedUser')
+      .insert({
+        user_ID : user_id,
+        name: name,
+        lastname: lastname,
+        personal_number: pnummer,
+        email: email,
+        phone_number: phone,
+        adress: adress,
+        city: city,
+        zip: zip
+      })
 
-      // TODO: Lägg till logik för att spara användare
-      // Save user to database
+      if (error) {
+        console.log("Error registering user:",error);
+        return res.status(500).json({ message: "User registration failed" });
+      }
+
       res.status(200).json({ message: "User registered successfully" });
+
     } catch (error) {
       res.status(500).json({ message: "User registration failed" });
     }
   };
 
-
-  export const loginUser = (req, res) => {
-    // TODO: Lägg till logik för att logga in användare
-  };
-
-  export const logoutUser = (req, res) => {
-    // TODO: Lägg till logik för att logga ut användare
-  };
 
 
 
--- index.js
+++ index.js
@@ -1,27 +1,58 @@
 import express from "express";
 import dotenv from "dotenv";
-import { createServer } from "http"; 
-import { Server } from "socket.io"; 
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+import cors from "cors";
+
+/*Import of the HTTP routers here  */
+import { authRouter } from "./routes/authRoutes.js";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: { origin: "*" } });
+const io = new Server(httpServer, {
+  cors: {
+    origin: [
+      "http://admin.socket.io",
+      "https://admin.socket.io",
+      "http://localhost:8080",
+      "https://localhost:8080",
+      "http://localhost:5173",
+      "https://localhost:5173",
+    ],
+    credentials: true,
+  },
+});
 
-dotenv.config();
-
-/*Import of the Routers  */
-import {authRouter} from "./routes/authRoutes.js";
+dotenv.config(); //Load environment variables from .env file
 
 //Middleware which parses incoming requests body from JSON into an object
 app.use(express.json());
+app.use(cors());
 
-/* HTTP endpoints */
+/* HTTPS endpoints here */
 app.use("/api/auth", authRouter);
 
+/*Websockets events here*/
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
 
-const PORT = process.env.PORT || 3000;
+  socket.on("disconnect", (socket) => {
+    console.log("A user disconnected: ", socket.id);
+  });
+
+  /*TODO: Implement the socket events here */
+});
+
+// Admin dashboard configuration for socket.io
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
 
 //Start the server with the http server since app.listen() would not work with socket.io
+const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Socket.io Admin UI is accessible at http://admin.socket.io`);
 });
