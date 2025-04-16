91cf2173a9f65a83660e69578bc04dadd24a8c3c
Jesper Sandberg Hesselgren <jespersh@kth.se>
2025-04-16 08:49:04 +0200

    Test of restricted zone endpoint

--- controller/restrictedZone-controller.js
+++ controller/restrictedZone-controller.js
@@ -5,16 +5,17 @@ const getZones = async (req, res) => {
     try {
         const { data, error } = await supabase
             .from('RestrictedZones')
-            .select('*')
+            .select('coorArray')
             .eq('zoneID', '0bcf1844-be48-45dd-af40-47d2b117580f')
 
-            console.log(data);
+            console.log(data[0].coorArray);
 
     } catch (error) {
         console.log("Error getting zones:", error);
         return res.status(500).json({ error: "Failed to get zones" });
     }
 }
+
 
 
 export {getZones}
